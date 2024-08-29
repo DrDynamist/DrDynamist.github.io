7d00ffade628d4d2aef6b0889c17982eb9ef306b
Nick <108819874+DrDynamist@users.noreply.github.com>
2024-08-28 23:41:07 -0400

    Update stupid.js

--- stupid.js
+++ stupid.js
@@ -1,8 +1,10 @@
 document.getElementById('tricky-text').addEventListener('mouseover', function() {
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
-    const offsetX = Math.random() * 100 - 50; 
-    const offsetY = Math.random() * 100 - 50; 
+    
+    const offsetX = Math.random() * 200 - 100;
+    const offsetY = Math.random() * 200 - 100;
+    
     const rect = this.getBoundingClientRect();
     
     let newLeft = rect.left + offsetX;
