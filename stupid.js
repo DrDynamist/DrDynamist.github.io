83b243ac2b0bd3b6bf4393b1ca75ff26211bc1f9
Nick <108819874+DrDynamist@users.noreply.github.com>
2024-08-28 22:21:14 -0400

    Create stupid.js

---
+++ stupid.js
@@ -0,0 +1,10 @@
+document.getElementById('tricky-text').addEventListener('mouseover', function() {
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    const newLeft = Math.random() * (viewportWidth - this.offsetWidth);
+    const newTop = Math.random() * (viewportHeight - this.offsetHeight);
+    this.style.position = 'absolute';
+    this.style.left = `${newLeft}px`;
+    this.style.top = `${newTop}px`;
+});
